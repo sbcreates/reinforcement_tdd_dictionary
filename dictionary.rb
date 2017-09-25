f9d10d6961bb30f5fd6a85aefd023fe70292b600
sbcreates <drawsarahb@gmail.com>
2017-09-25 14:28:01 -0400

    Wrote add method and tested it can add keyword and definition and keyword without a definition

--- dictionary.rb
+++ dictionary.rb
@@ -1,7 +1,23 @@
 class Dictionary
 
   def initialize
+    @entries = {}
   end
 
+  def entries
+    @entries
+  end
+
+  def add(entry = {})
+    if entry.class == String
+      @entries[entry] = nil
+    else
+      entry.each { |keyword, definition| @entries[keyword] = definition }
+    end
+  end
+
+  def keywords
+    @entries.keys
+  end
 
 end
